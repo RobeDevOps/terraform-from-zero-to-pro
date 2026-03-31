f64d4b392afa7651be67d8ca291d3346cf6b2d94
wow <wow@DevBobs.lan>
2026-03-30 22:00:08 -0400

    todos para el nuevo episodio 003

---
+++ 003-Basic-Variables/main.tf
@@ -0,0 +1,16 @@
+resource "azurerm_resource_group" "example" {
+  name     = "example-resources"
+  location = "East US"
+}
+
+resource "azurerm_storage_account" "example" {
+  name                     = "stagexample02271999"
+  resource_group_name      = azurerm_resource_group.example.name
+  location                 = azurerm_resource_group.example.location
+  account_tier             = "Standard"
+  account_replication_type = "LRS"
+
+  tags = {
+    environment = "dev"
+  }
+}
